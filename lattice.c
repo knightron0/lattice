d9dfc5ca528e897f219be82866abc580b5d89771
Madhav Shekhar Sharma <33770878+mdvsh@users.noreply.github.com>
2024-05-20 13:58:29 +0530

    [chore]: implement isomerize and add, need cpu/gpu add utils

--- lattice.c
+++ lattice.c
@@ -1,5 +1,8 @@
 #include <math.h>
 #include <lattice.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 Lattice *crystallize(float* data, int* shapes, int ndim, char* kahan) {
   Lattice *lattice = (Lattice *) malloc(sizeof(Lattice));
@@ -28,11 +31,96 @@ void bhej(Lattice* lattice, char* kahan) {
   
 }
 
-Lattice* isomerize(Lattice *lattice, int* new_shapes) {
-  char *kahan = lattice->kahan;
-  if (n)
+Lattice* isomerize(Lattice *lattice, int new_ndim, int* new_shapes) {
+  int* shapes = (int*)malloc(new_ndim * sizeof(int));
+  if (!shapes) {
+    fprintf(stderr, "malloc failed for shapes\n");
+    exit(1);
+  } else {
+    for (int i = 0; i < new_ndim; i++) {
+      shapes[i] = new_shapes[i];
+    }
+  }
+
+  char* kahan = (char*)malloc(strlen(lattice->kahan) + 1);
+  if (!kahan) {
+    fprintf(stderr, "malloc failed for device\n");
+    exit(1);
+  } else {
+    strcpy(kahan, lattice->kahan);
+  }
+
+  int kitna = 1;
+  for (int i = 0; i < new_ndim; i++) {
+    kitna *= shapes[i];
+  }
+
+  if (kitna != lattice->kitna) {
+    fprintf(stderr, "size mismatch\n");
+    exit(1);
+  }
+
+  int* stride = (int*)malloc(new_ndim * sizeof(int));
+  stride[new_ndim - 1] = 1;
+  for (int i = new_ndim - 2; i >= 0; i--) {
+    stride[i] = stride[i + 1] * shapes[i + 1]; 
+  }
+
+  free(shapes);
+  free(stride);
+
+  lattice->shapes = shapes;
+  lattice->stride = stride;
+  lattice->ndim = new_ndim;
+
 }
 
+Lattice* add(Lattice* lattice1, Lattice* lattice2) {
+  if (lattice1->ndim != lattice2->ndim || lattice1->kitna != lattice2->kitna) {
+    fprintf(stderr, "cannot add, ndims || size doesn't match.\n", lattice1->ndim, lattice2->ndim, ".\t", lattice1->kitna, lattice2->kitna);
+    exit(1);
+  }
 
+  if (strcmp(lattice1->kahan, lattice2->kahan) != 0) {
+    fprintf(stderr, "cannot add, device (kahan) doesn't match.\n", lattice1->kahan,
+            lattice2->kahan);
+    exit(1);
+  }
 
+  for (int i = 0; i < lattice1->ndim; i++) {
+    if (lattice1->shapes[i] != lattice2->shapes[i]) {
+      fprintf(stderr, "cannot add, shape mismatch\n");
+      exit(1);
+    }
+  }
 
+  char* kahan = (char*)malloc(strlen(lattice1->kahan) + 1);
+  if (!kahan) {
+    fprintf(stderr, "malloc failed for device\n");
+    exit(1);
+  } else {
+    strcpy(kahan, lattice1->kahan);
+  }
+
+  int ndim = lattice1->ndim;
+  int* shapes = (int*)malloc(ndim * sizeof(int));
+  if (!shapes) {
+    fprintf(stderr, "malloc failed for addition\n");
+    exit(1);
+  }
+
+  if (strcmp(lattice1->kahan, "cuda") == 0) {
+    float* added_data;
+    cudaMalloc((void**)&added_data, lattice1->kitna * sizeof(float));
+    // add_tensor_cuda(lattice1, lattice2, added_data);
+    return crystallize(added_data, shapes, ndim, kahan);
+  } else {
+    float* added_data = (float*)malloc(lattice1->kitna * sizeof(float));
+    if (!added_data) {
+      fprintf(stderr, "malloc failed\n");
+      exit(1);
+    }
+    // add_tensor_cpu(lattice1, lattice2, added_data);
+    return create_tensor(added_data, shapes, ndim, kahan);
+  }
+}
