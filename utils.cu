85bfff665bc25804d5b3382d9c12ff2fa693c8b8
Somesh Kar <somesh.kar@gmail.com>
2024-05-20 13:45:54 +0530

    impl(bhej util funcs)

--- utils.cu
+++ utils.cu
@@ -1,12 +1,30 @@
 #include <lattice.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-__host__ void cpu_to_cuda(Lattice* lattice) {
-  float *kya_temp;
+void cpu_to_cuda(Lattice* lattice) {
+  float *data_temp;
 
-  cudaMalloc(&kya_temp, lattice->kitna * sizeof(int));
-  cudaMemcpy(kya_temp, lattice->data, lattice->dat);
+  cudaMalloc((void **)&data_temp, lattice->kitna * sizeof(int));
+  cudaMemcpy(data_temp, lattice->data, lattice->data * sizeof(int), cudaMemcpyHostToDevice);
+
+  lattice->data = data_temp;
+
+  lattice->kahan = (char*)malloc(strlen("cuda") + 1);
+  strcpy(lattice->kahan, "cuda"); 
 }
 
-__host__ void cpu_to_cuda(Lattice* lattice) {
+void cpu_to_cuda(Lattice* lattice) {
+  float* data_tmp = (float*)malloc(lattice->kitna * sizeof(int));
 
+  cudaMemcpy(data_tmp, lattice->data, lattice->kitna * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaFree(lattice->data);
+
+  lattice->data = data_tmp;
+
+  lattice->kahan = (char*)malloc(strlen("cpu") + 1);
+  strcpy(lattice->kahan, "cpu"); 
+
+  printf("Sent your lattice to: %s\n", lattice->kahan);
 }
