dab785f18b9124afd1b14a9d87231f5b56628665
Somesh Kar <somesh.kar@gmail.com>
2024-05-20 13:49:17 +0530

    chore(bhej function def)

--- lattice.c
+++ lattice.c
@@ -1,38 +1,35 @@
 #include <math.h>
 #include <lattice.h>
 
-Lattice *crystallize(float* data, int* shapes, int ndim, char* kahan) {
-  Lattice *lattice = (Lattice *) malloc(sizeof(Lattice));
+Lattice *crystallize(float *data, int *shapes, int ndim, char *kahan) {
+  Lattice *lattice = (Lattice *)malloc(sizeof(Lattice));
   lattice->data = data;
   lattice->shapes = shapes;
   lattice->ndim = ndim;
 
   lattice->kitna = 1;
-  for (int i = 0; i < ndim; i++) lattice->kitna *= lattice->shapes[i];
+  for (int i = 0; i < ndim; i++)
+    lattice->kitna *= lattice->shapes[i];
 
   lattice->kahan = kahan;
   int mul = 1;
-  for (int i = ndim - 1; i >= 0; i--) {
+  for (int i = ndim - 1; i >= 0; i--)
+  {
     lattice->stride[i] = mul;
     mul *= lattice->shapes[i];
   }
   return lattice;
-  
 }
 
-
-
-
-
-void bhej(Lattice* lattice, char* kahan) {
-  
+void bhej(Lattice *lattice, char *kahan) {
+  if (strcmp(kahan, "cuda") == 0 && strcmp(lattice->kahan, "cpu") == 0) {
+    cpu_to_cuda(lattice);
+  } else if (strcmp(kahan, "cpu") == 0 && strcmp(lattice->kahan, "cuda") == 0) {
+    cuda_to_cpu(lattice);
+  }
 }
 
-Lattice* isomerize(Lattice *lattice, int* new_shapes) {
+Lattice *isomerize(Lattice *lattice, int *new_shapes) {
   char *kahan = lattice->kahan;
   if (n)
 }
-
-
-
-
