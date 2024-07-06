63ff37e0f5525f3e29340e2d3edc275aa7f5ede0
knightron0 <mangla@purdue.edu>
2024-07-05 20:07:35 -0400

    struct -> class

--- src/lattice.cu
+++ src/lattice.cu
@@ -4,67 +4,63 @@
 #include <stdlib.h>
 #include <string.h>
 
-Lattice* crystallize(int *shapes, int ndim) {
-  /*
-    shapes: an array of integers representing the shape of the lattice
-    ndim: an integer representing the number of dimensions in the lattice
-
-    returns a Lattice with random floats in the range [0, 1]
-  */
-
-  Lattice* lattice = (Lattice *)malloc(sizeof(Lattice));
+Lattice::Lattice(int *shapes, int ndim) {
+  this->shapes = shapes;
+  this->ndim = ndim;
   
-  lattice->shapes = shapes;
-  lattice->ndim = ndim;
-  
-  lattice->size = 1;
-  for (int i = 0; i < ndim; i++) lattice->size *= shapes[i];
+  this->size = 1;
+  for (int i = 0; i < ndim; i++) this->size *= shapes[i];
 
-  lattice->data = (float*)malloc(lattice->size * sizeof(float));
-  for (int i = 0; i < lattice->size; i++) lattice->data[i] = (float)rand() / (float)RAND_MAX;
+  this->data = (float*)malloc(this->size * sizeof(float));
+  for (int i = 0; i < this->size; i++) this->data[i] = (float)rand() / (float)RAND_MAX;
 
-  lattice->stride = (int *)malloc(lattice->ndim * sizeof(int));
-  lattice->stride[ndim - 1] = 1;
-  for (int i = ndim - 2; i >= 0; i--) lattice->stride[i] = lattice->stride[i + 1] * lattice->shapes[i+1];
+  this->stride = (int *)malloc(this->ndim * sizeof(int));
+  this->stride[ndim - 1] = 1;
+  for (int i = ndim - 2; i >= 0; i--) this->stride[i] = this->stride[i + 1] * this->shapes[i+1];
 
-  lattice->where = (char *)"cuda";
-
-  return lattice;
+  this->where = (char *)"cuda";
 }
 
-float get_value(Lattice* lattice, int *indices) {
-  if (sizeof(indices) / sizeof(indices[0]) != lattice->ndim) {
+Lattice::~Lattice() {
+  free(this->data);
+  free(this->shapes);
+  free(this->stride);
+  free(this->where);
+}
+
+float Lattice::get(int *indices) {
+  if (sizeof(indices) / sizeof(indices[0]) != this->ndim) {
     printf("Error: Size of indices does not match the number of dimensions in the lattice.\n");
     return 0.0f;
   }
   int index = 0;
-  for (int i = 0; i < lattice->ndim; i++) {
-    index += indices[i] * lattice->stride[i];
+  for (int i = 0; i < this->ndim; i++) {
+    index += indices[i] * this->stride[i];
   }
-  return lattice->data[index];
+  return this->data[index];
 }
 
 int main() {
   int shapes[2] = {10, 10};
   int ndim = 2;
-  Lattice* a = crystallize(shapes, ndim);
+  Lattice a = Lattice(shapes, ndim);
   printf("Lattice a: \n");
   int indices[2] = {0, 0};
   for (int i = 0; i < shapes[0]; i++) {
     for (int j = 0; j < shapes[0]; j++) {
       indices[0] = i;
       indices[1] = j;
-      printf("%f ", get_value(a, indices));
+      printf("%f ", a.get(indices));
     }
     printf("\n");
   }
-  Lattice* b = crystallize(shapes, ndim);
+  Lattice b = Lattice(shapes, ndim);
   printf("Lattice b: \n");
   for (int i = 0; i < shapes[0]; i++) {
     for (int j = 0; j < shapes[0]; j++) {
       indices[0] = i;
       indices[1] = j;
-      printf("%f ", get_value(b, indices));
+      printf("%f ", b.get(indices));
     }
     printf("\n");
   }
--- src/lattice.cuh
+++ src/lattice.cuh
@@ -1,16 +1,19 @@
 #ifndef LATTICE_CUH
 #define LATTICE_CUH
 
-typedef struct {
+class Lattice {
+public:
     float *data; // data pointer
     int* shapes; // shape of the lattice
     int* stride; // stride of the lattice
     int ndim; // number of dimensions
     int size; // size of the lattice (= dimension product)
     char* where; // location of the lattice
-} Lattice;
 
+    Lattice(int *shapes, int ndim);
+    ~Lattice();
+    float get(int *indices);
+};
 
-Lattice* crystallize(int *shapes, int ndim); 
 
 #endif /* LATTICE_CUH */
