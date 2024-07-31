125dd0a576fc35002bd789dff5733fae3530bd58
Somesh Kar <somesh.kar@gmail.com>
2024-07-31 12:27:36 +0530

    fix(build): uncomment main

--- tests/feedforward.cu
+++ tests/feedforward.cu
@@ -3,11 +3,11 @@
 #include "../src/arch/mlp.cuh"
 #include "../src/lattice.cuh"
 
-// int main() {
-//   Linear l1 = Linear(784, 500, 1, (char *)"cuda");
-//   int x_shape[2] = {1, 784};
-//   Lattice x = Lattice(x_shape, 2, ONES);
-//   MLP mlp = MLP(3, new int[3]{500, 64, 32}, new ActivationFunction[4]{RELU, RELU, RELU, SOFTMAX});
-//   mlp.forward(x);
-//   return 0;
-// }
+int main() {
+  Linear l1 = Linear(784, 500, 1, (char *)"cuda");
+  int x_shape[2] = {1, 784};
+  Lattice x = Lattice(x_shape, 2, ONES);
+  MLP mlp = MLP(3, new int[3]{500, 64, 32}, new ActivationFunction[4]{RELU, RELU, RELU, SOFTMAX});
+  mlp.forward(x);
+  return 0;
+}
